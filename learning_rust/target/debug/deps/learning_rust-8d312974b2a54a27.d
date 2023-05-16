757eef63322f3d5d74160220061ae492a69ca8a6
N <nipz@users.noreply.github.com>
2023-05-17 00:01:56 +0100

    Learning how to use cargo in rust

---
+++ learning_rust/target/debug/deps/learning_rust-8d312974b2a54a27.d
@@ -0,0 +1,5 @@
+/Users/exo/Desktop/Learning_Rust_Public/learning_rust/target/debug/deps/learning_rust-8d312974b2a54a27: src/main.rs
+
+/Users/exo/Desktop/Learning_Rust_Public/learning_rust/target/debug/deps/learning_rust-8d312974b2a54a27.d: src/main.rs
+
+src/main.rs:
---
+++ learning_rust/target/debug/learning_rust.d
@@ -0,0 +1,1 @@
+/Users/exo/Desktop/Learning_Rust_Public/learning_rust/target/debug/learning_rust: /Users/exo/Desktop/Learning_Rust_Public/learning_rust/src/main.rs
